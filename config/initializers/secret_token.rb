4dd6d05d35bb138877ad335bfc266984e2551577
Sebastian Single <sebastian.single@meetnow.eu>
2013-03-11 06:58:18 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '61ab7485bfe09fe7082c14343c6da16f1110709fef2efa18e8521216aed6a0ec1d0ff6a577b65ec45261eb226d4782a634ec691daa25748764d61759ec8ed6f3'
